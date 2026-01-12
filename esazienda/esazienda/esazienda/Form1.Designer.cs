675ad59ce8a4ca5488be831b209ede2801cda361
buizza <brwsuipa@gmail.com>
2026-01-12 11:14:52 +0100

    commit1

---
+++ esazienda/esazienda/esazienda/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace esazienda
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            button1 = new Button();
+            dateTimePicker2 = new DateTimePicker();
+            dateTimePicker1 = new DateTimePicker();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            tabPage2 = new TabPage();
+            dateTimePicker4 = new DateTimePicker();
+            dateTimePicker3 = new DateTimePicker();
+            textBox8 = new TextBox();
+            textBox7 = new TextBox();
+            button3 = new Button();
+            textBox6 = new TextBox();
+            button2 = new Button();
+            textBox5 = new TextBox();
+            tabPage3 = new TabPage();
+            button4 = new Button();
+            dataGridView1 = new DataGridView();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Location = new Point(10, 10);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(778, 428);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(dateTimePicker2);
+            tabPage1.Controls.Add(dateTimePicker1);
+            tabPage1.Controls.Add(textBox4);
+            tabPage1.Controls.Add(textBox3);
+            tabPage1.Controls.Add(textBox2);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(770, 400);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(79, 293);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "insserimento turni ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(79, 252);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(78, 210);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(79, 165);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(79, 124);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(79, 80);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(79, 38);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(dateTimePicker4);
+            tabPage2.Controls.Add(dateTimePicker3);
+            tabPage2.Controls.Add(textBox8);
+            tabPage2.Controls.Add(textBox7);
+            tabPage2.Controls.Add(button3);
+            tabPage2.Controls.Add(textBox6);
+            tabPage2.Controls.Add(button2);
+            tabPage2.Controls.Add(textBox5);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(770, 400);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker4
+            // 
+            dateTimePicker4.Location = new Point(520, 242);
+            dateTimePicker4.Name = "dateTimePicker4";
+            dateTimePicker4.Size = new Size(200, 23);
+            dateTimePicker4.TabIndex = 7;
+            // 
+            // dateTimePicker3
+            // 
+            dateTimePicker3.Location = new Point(520, 213);
+            dateTimePicker3.Name = "dateTimePicker3";
+            dateTimePicker3.Size = new Size(200, 23);
+            dateTimePicker3.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(520, 175);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(520, 146);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 4;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(390, 208);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(390, 169);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(86, 154);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(73, 113);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(button4);
+            tabPage3.Controls.Add(dataGridView1);
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(770, 400);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "tabPage3";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(165, 148);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 1;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(324, 95);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.Size = new Size(240, 150);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Button button1;
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private TabPage tabPage2;
+        private Button button2;
+        private TextBox textBox5;
+        private Button button3;
+        private TextBox textBox6;
+        private DateTimePicker dateTimePicker4;
+        private DateTimePicker dateTimePicker3;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TabPage tabPage3;
+        private Button button4;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ esazienda/esazienda/esazienda/Form1.cs
@@ -0,0 +1,178 @@
+
+
+using MySql;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace esazienda
+{
+    public partial class Form1 : Form
+    {
+
+
+        private string connectionString;
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = "Server=192.168.103.51;Port=3306;Uid=4IB;Pwd=4ibroot;DataBase=25_26_4IB_GRUPPO_B";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int ID_turno));
+            string Nome_turno = textBox2.Text;
+            string descrizione = textBox3.Text;
+            DateTime Ora_inizio = dateTimePicker1.Value;
+            DateTime Ora_fINE = dateTimePicker2.Value;
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "INSERT INTO Turni (ID_Turno, Nome_Turno, Ora_Inizio, Ora_Fine, Descrizione) " +
+                              "VALUES (@id, @nome, @inizio, @fine, @desc)";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+                    cmd.Parameters.AddWithValue("@nome", textBox2.Text);
+                    cmd.Parameters.AddWithValue("@inizio", dateTimePicker1.Value.ToString("HH:mm:ss"));
+                    cmd.Parameters.AddWithValue("@fine", dateTimePicker2.Value.ToString("HH:mm:ss"));
+                    cmd.Parameters.AddWithValue("@desc", textBox3.Text);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Fatto! Dati salvati.");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "DELETE FROM Turni WHERE ID_Turno = @id";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", int.Parse(textBox5.Text));
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+
+            MessageBox.Show($"sono stati cancellati del tipo turni");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int idDaModificare = int.Parse(textBox6.Text);
+
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    // Query per modificare il turno
+                    string query = @"UPDATE Turni SET 
+                            Nome_Turno = @nome,
+                            Ora_Inizio = @inizio,
+                            Ora_Fine = @fine,
+                            Descrizione = @desc
+                            WHERE ID_Turno = @id";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@id", idDaModificare);
+                        cmd.Parameters.AddWithValue("@nome", textBox7.Text);
+                        cmd.Parameters.AddWithValue("@inizio", dateTimePicker3.Value.ToString("HH:mm:ss"));
+                        cmd.Parameters.AddWithValue("@fine", dateTimePicker4.Value.ToString("HH:mm:ss"));
+                        cmd.Parameters.AddWithValue("@desc", textBox8.Text);
+
+                        int righeModificate = cmd.ExecuteNonQuery();
+
+                        if (righeModificate > 0)
+                        {
+                            MessageBox.Show("Turno modificato con successo!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Nessun turno trovato con questo ID");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore: " + ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT * FROM Turni";
+
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dataGridView1.DataSource = dataTable;
+                }
+            }
+
+            MessageBox.Show($"Caricati {dataGridView1.RowCount - 1} turni");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            // Prendi la riga cliccata
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+            // Metti i dati nelle textbox
+            textBox6.Text = row.Cells["ID_Turno"].Value?.ToString();        // ID
+            textBox7.Text = row.Cells["Nome_Turno"].Value?.ToString();     // Nome
+            textBox8.Text = row.Cells["Descrizione"].Value?.ToString();    // Descrizione
+
+            // Per le ore (se sono stringhe)
+            string oraInizio = row.Cells["Ora_Inizio"].Value?.ToString();
+            string oraFine = row.Cells["Ora_Fine"].Value?.ToString();
+
+            if (!string.IsNullOrEmpty(oraInizio) && DateTime.TryParse(oraInizio, out DateTime dtInizio))
+                dateTimePicker3.Value = dtInizio;
+
+            if (!string.IsNullOrEmpty(oraFine) && DateTime.TryParse(oraFine, out DateTime dtFine))
+                dateTimePicker4.Value = dtFine;
+
+            // Vai alla tabPage3
+            tabControl1.SelectedTab = tabPage2;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //if (e.RowIndex < 0) return;
+
+            //// Prendi l'ID dalla prima colonna
+            //string id = dataGridView1.Rows[e.RowIndex].Cells[0].Value?.ToString();
+
+            //if (!string.IsNullOrEmpty(id))
+            //{
+            //    // Metti l'ID nella textBox1
+            //    textBox1.Text = id;
+
+            //    // Vai alla tabPage3
+            //    tabControl1.SelectedTab = tabPage2;
+            //}
+        }
+    }
+}
